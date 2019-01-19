d07c99b44f77d84a3d20d4afe53586610a4a6dc7
James <13631413532@163.com>
2019-01-19 22:15:21 +0800

    增加主页侧滑栏

--- app/src/main/java/com/kt/james/wmsforandroid/business/main/MainActivity.java
+++ app/src/main/java/com/kt/james/wmsforandroid/business/main/MainActivity.java
@@ -1,35 +1,61 @@
 package com.kt.james.wmsforandroid.business.main;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.kt.james.wmsforandroid.R;
-import com.kt.james.wmsforandroid.base.BaseActivity;
 import com.kt.james.wmsforandroid.business.utils.WmsSpManager;
 import com.kt.james.wmsforandroid.databinding.ActivityMainBinding;
+import com.kt.james.wmsforandroid.databinding.LayoutNavMainBinding;
 import com.kt.james.wmsforandroid.utils.ResourceUtil;
+import com.kt.james.wmsforandroid.view.StatusBarUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Route(path = "/main/main_activity", name = "主页面")
-public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MainPageAdapter mPageAdapter;
+    private ActivityMainBinding bindingView;
+    private LayoutNavMainBinding navBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle(ResourceUtil.getString(R.string.main_title));
-        initView();
+        bindingView = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initStatusView();
+        initDrawerLayout();
+        initRecycleView();
         initMenu();
-        showContentView();
-        bindingView.setViewModel(viewModel);
     }
 
-    private void initView() {
+    private void initStatusView() {
+        ViewGroup.LayoutParams layoutParams = bindingView.viewStatus.getLayoutParams();
+        layoutParams.height = StatusBarUtil.getStatusBarHeight(this);
+        bindingView.viewStatus.setLayoutParams(layoutParams);
+        StatusBarUtil.setColorNoTranslucentForDrawerLayout(MainActivity.this, bindingView.drawerLayout,
+                ResourceUtil.getColor(R.color.colorTheme));
+    }
+
+    private void initDrawerLayout() {
+        bindingView.navView.inflateHeaderView(R.layout.layout_nav_main);
+        View headerView = bindingView.navView.getHeaderView(0);
+        navBinding = DataBindingUtil.bind(headerView);
+        navBinding.llNavAbout.setOnClickListener(this);
+        navBinding.llNavExit.setOnClickListener(this);
+        navBinding.llNavLogin.setOnClickListener(this);
+        bindingView.ivTitleMenu.setOnClickListener(this);
+    }
+
+    private void initRecycleView() {
         mPageAdapter = new MainPageAdapter(this);
         bindingView.rvMainPage.setAdapter(mPageAdapter);
         bindingView.rvMainPage.setLayoutManager(new GridLayoutManager(this, 3));
@@ -54,5 +80,15 @@ public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBindin
     protected void onDestroy() {
         super.onDestroy();
         WmsSpManager.setIsLogin(false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_title_menu:
+                // 开启菜单
+                bindingView.drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
     }
 }
