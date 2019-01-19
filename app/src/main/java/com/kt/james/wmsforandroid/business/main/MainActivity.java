91bd7c682d45889bde22458564bc1021b1467804
James <13631413532@163.com>
2019-01-19 19:39:15 +0800

    修改主页样式，增加模块入口

--- app/src/main/java/com/kt/james/wmsforandroid/business/main/MainActivity.java
+++ app/src/main/java/com/kt/james/wmsforandroid/business/main/MainActivity.java
@@ -16,27 +16,38 @@ import java.util.List;
 @Route(path = "/main/main_activity", name = "主页面")
 public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
 
+    private MainPageAdapter mPageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(ResourceUtil.getString(R.string.main_title));
         initView();
+        initMenu();
         showContentView();
         bindingView.setViewModel(viewModel);
     }
 
     private void initView() {
-        MainPageAdapter adapter = new MainPageAdapter(this);
-        bindingView.rvMainPage.setAdapter(adapter);
+        mPageAdapter = new MainPageAdapter(this);
+        bindingView.rvMainPage.setAdapter(mPageAdapter);
         bindingView.rvMainPage.setLayoutManager(new GridLayoutManager(this, 3));
-        MainPageBean bean;
+    }
+
+    private void initMenu() {
         List<MainPageBean> list = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            bean = new MainPageBean(R.drawable.icon_shelf, "上架");
-            list.add(bean);
-        }
-        adapter.addAll(list);
+        list.add(new MainPageBean(R.drawable.icon_shelf,
+                ResourceUtil.getString(R.string.main_module_shelf)));
+        list.add(new MainPageBean(R.drawable.icon_down,
+                ResourceUtil.getString(R.string.main_module_down)));
+        list.add(new MainPageBean(R.drawable.icon_replenish,
+                ResourceUtil.getString(R.string.main_module_report)));
+        list.add(new MainPageBean(R.drawable.icon_report,
+                ResourceUtil.getString(R.string.main_module_report)));
+        list.add(new MainPageBean(R.drawable.icon_input,
+                ResourceUtil.getString(R.string.main_module_input)));
+        mPageAdapter.addAll(list);
     }
 
     @Override
